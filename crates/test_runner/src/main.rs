0d7724c20034c5b33712ae2202583eaf1f489a73
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2025-10-04 08:17:43 -0400

    test code

--- crates/test_runner/src/main.rs
+++ crates/test_runner/src/main.rs
@@ -17,9 +17,13 @@ fn main()
     // let verse = VerseId::from_str("Gen.2.10").unwrap();
     // let fetch = package.fetch(verse, "KJV");
 
-    let bible = package.get_mod("KJV").unwrap().as_bible().unwrap();
-    let abbrev = bible.get_abbreviated_book(OsisBook::John);
+    // let bible = package.get_mod("KJV").unwrap().as_bible().unwrap();
+    // let abbrev = bible.get_abbreviated_book(OsisBook::John);
 
-    println!("{:#?}", abbrev);
+    // println!("{:#?}", abbrev);
+
+    let notebook = package.get_mod("Test Notebook").unwrap().as_notebook().unwrap();
+    let entry = notebook.entries.iter().find(|e| e.id() == 0).unwrap();
+    println!("{:#?}", entry);
 }
 
